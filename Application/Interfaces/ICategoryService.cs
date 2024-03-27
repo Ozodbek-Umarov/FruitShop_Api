5041df5658679b6d8553ab8e64c1f02abeb8bf23
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-03-27 09:58:31 +0500

    Interfaces Added

---
+++ Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.CategoryDtos;
+
+namespace Application.Interfaces;
+
+public interface ICategoryService
+{
+    Task<List<CategoryDto>> GetAllAsync();
+    Task<CategoryDto> GetByIdAsync(int id);
+    Task AddAsync(AddCategoryDto dto);
+    Task UpdateAsync(CategoryDto dto);
+    Task DeleteAsync(int id);
+}
---
+++ Application/Interfaces/IFruitService.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.FruitDtos;
+
+namespace Application.Interfaces;
+
+public interface IFruitService
+{
+    Task<List<FruitDto>> GetAllAsync();
+    Task<FruitDto> GetByIdAsync(int id);
+    Task AddAsync(AddFruitDto dto);
+    Task UpdateAsync(FruitDto dto);
+    Task DeleteAsync(int id);
+}
